61638e912d91bf2489d21c3d72cea2a4156df91b
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2025-12-14 02:26:46 +0100

    Update offline.js

--- api/offline.js
+++ api/offline.js
@@ -15,14 +15,18 @@ export default async function handler(req, res) {
   } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: "mail.privateemail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  host: "mail.privateemail.com",
+  port: 465,
+  secure: true, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, 
+  },
+});
+
 
   try {
     await transporter.sendMail({
